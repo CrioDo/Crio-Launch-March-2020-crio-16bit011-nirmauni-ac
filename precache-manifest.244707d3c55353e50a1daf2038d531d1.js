7ed845f81c90db162159f499aee7429c4cb60c96
Somnath <somnath@Somnaths-MacBook-Air.local>
2020-04-27 13:46:44 +0530

    Updates

--- precache-manifest.244707d3c55353e50a1daf2038d531d1.js
+++ precache-manifest.244707d3c55353e50a1daf2038d531d1.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "def1f3ae7e5bc2d39c278d10ea9e177d",
+    "revision": "bb9defca581b8fee9503e78385aefb79",
     "url": "/Crio-Launch-March-2020-crio-16bit011-nirmauni-ac/index.html"
   },
   {
-    "revision": "cb1180f270b1df6cab69",
+    "revision": "7b5e527c94d559277d6b",
     "url": "/Crio-Launch-March-2020-crio-16bit011-nirmauni-ac/static/css/2.57ddf7ad.chunk.css"
   },
   {
-    "revision": "5b4aebcc8f7b8183f80a",
+    "revision": "744d614fb3e7d2308f6f",
     "url": "/Crio-Launch-March-2020-crio-16bit011-nirmauni-ac/static/css/main.5db1380d.chunk.css"
   },
   {
-    "revision": "cb1180f270b1df6cab69",
-    "url": "/Crio-Launch-March-2020-crio-16bit011-nirmauni-ac/static/js/2.cbd7bb14.chunk.js"
+    "revision": "7b5e527c94d559277d6b",
+    "url": "/Crio-Launch-March-2020-crio-16bit011-nirmauni-ac/static/js/2.57f450b2.chunk.js"
   },
   {
     "revision": "d466ab9648f013dea081956fc7c904f3",
-    "url": "/Crio-Launch-March-2020-crio-16bit011-nirmauni-ac/static/js/2.cbd7bb14.chunk.js.LICENSE.txt"
+    "url": "/Crio-Launch-March-2020-crio-16bit011-nirmauni-ac/static/js/2.57f450b2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5b4aebcc8f7b8183f80a",
-    "url": "/Crio-Launch-March-2020-crio-16bit011-nirmauni-ac/static/js/main.3e20bf14.chunk.js"
+    "revision": "744d614fb3e7d2308f6f",
+    "url": "/Crio-Launch-March-2020-crio-16bit011-nirmauni-ac/static/js/main.f95604c5.chunk.js"
   },
   {
-    "revision": "a9002b69f55f3a756054",
-    "url": "/Crio-Launch-March-2020-crio-16bit011-nirmauni-ac/static/js/runtime-main.7bba79e7.js"
+    "revision": "5d03b02822889b604bfb",
+    "url": "/Crio-Launch-March-2020-crio-16bit011-nirmauni-ac/static/js/runtime-main.ffeb090b.js"
   },
   {
     "revision": "c9536e0497f939475da3110091e1a10d",
